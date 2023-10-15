6eeed40c2e09ad756f0326e09e92b3249e522210
fj.perezpujalte <javiperpu@gmail.com>
2023-10-15 13:41:42 +0200

    Repositorio json casi completo

--- estaciones-BarrenecheaMartinez/src/main/java/repositorio/RepositorioJSON.java
+++ estaciones-BarrenecheaMartinez/src/main/java/repositorio/RepositorioJSON.java
@@ -1,5 +1,94 @@
 package repositorio;
 
-public abstract class RepositorioJSON<T extends Identificable> implements RepositorioString<T> {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
 
+import javax.json.bind.Jsonb;
+import javax.json.bind.spi.JsonbProvider;
+
+public abstract class RepositorioJSON<T extends Identificable> implements RepositorioString<T>
+{
+
+	@Override
+	public String add(T entity) throws RepositorioException {
+		// TODO Auto-generated method stub
+		Jsonb contexto = JsonbProvider.provider().create().build();
+		String entityJson = contexto.toJson(entity);
+		try {
+			FileWriter fichero = new FileWriter("json/"+entity.getId()+".json");
+			fichero.write(entityJson);
+			fichero.close();
+			return entity.getId();
+		} catch (IOException e) {
+			throw new RepositorioException("Fallo al guardar "+entity.getId(),e);
+		}
+		
+	}
+
+	@Override
+	public void update(T entity) throws RepositorioException, EntidadNoEncontrada
+	{
+		String rutaFichero = "json/"+entity.getId()+".json";
+		File fichero = new File(rutaFichero);
+		if(!fichero.exists())
+			throw new EntidadNoEncontrada("No se encuentra la entidad con id "+entity.getId());
+		Jsonb contexto = JsonbProvider.provider().create().build();
+		String entityJson = contexto.toJson(entity);
+		try {
+			FileWriter nuevofichero = new FileWriter("json/"+entity.getId()+".json");
+			nuevofichero.write(entityJson);
+			nuevofichero.close();
+		} catch (IOException e) {
+			throw new RepositorioException("Fallo al guardar "+entity.getId(),e);
+		}
+	}
+
+	@Override
+	public void delete(T entity) throws RepositorioException, EntidadNoEncontrada {
+		String id = entity.getId();
+		// query document exists
+		File fichero = new File("json/" + id + ".json");
+		if (!fichero.exists()) {
+			throw new EntidadNoEncontrada("No existe: " + id);
+		}
+		// delete from document
+		fichero.delete();
+		if(fichero.exists())
+			throw new RepositorioException("Fallo al eliminar "+entity.getId());
+		
+	}
+	
+	public abstract Class<T> getClase();
+
+	@Override
+	public T getById(String id) throws RepositorioException, EntidadNoEncontrada {
+		String rutaFichero = "json/"+id+".json";
+		File fichero = new File(rutaFichero);
+		try {
+			Reader entrada = new FileReader(fichero);
+			Jsonb contexto = JsonbProvider.provider().create().build();
+			T entity = contexto.fromJson(entrada, getClase());
+			return entity;
+		} catch (FileNotFoundException e) {
+			throw new EntidadNoEncontrada("No se encuentra la entidad con id "+id);
+		}
+	}
+
+	@Override
+	public List<T> getAll() throws RepositorioException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getIds() throws RepositorioException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
--- estaciones-BarrenecheaMartinez/src/main/java/repositorio/RepositorioString.java
+++ estaciones-BarrenecheaMartinez/src/main/java/repositorio/RepositorioString.java
@@ -4,6 +4,7 @@ package repositorio;
  * Extensión de la interfaz repositorio para concretar
  * el uso de cadenas como identificadores
  */
-public interface RepositorioString<T> extends Repositorio<T, String> {
+public interface RepositorioString<T> extends Repositorio<T, String>
+{
 
 }
