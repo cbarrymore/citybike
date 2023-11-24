90ec275677db1e5615a693d56c6160793ae5776a
fj.perezpujalte <javiperpu@gmail.com>
2023-11-24 19:35:15 +0100

    Tests casi terminados, falta arreglar la base de datos sql y comprobar que los tests den bien

--- estaciones-BarrenecheaMartinez/src/test/java/aadd/estaciones_BarrenecheaMartinez/TestServicioEstaciones.java
+++ estaciones-BarrenecheaMartinez/src/test/java/aadd/estaciones_BarrenecheaMartinez/TestServicioEstaciones.java
@@ -3,10 +3,15 @@ package aadd.estaciones_BarrenecheaMartinez;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.math.BigDecimal;
+import java.util.Set;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import bicis.modelo.Bici;
 import estaciones.modelo.Estacion;
@@ -18,18 +23,20 @@ import repositorio.FactoriaRepositorios;
 import repositorio.Repositorio;
 import repositorio.RepositorioException;
 import servicio.FactoriaServicios;
+import sitioTuristico.modelo.SitioTuristico;
 
+@TestMethodOrder(OrderAnnotation.class)
 class TestServicioEstaciones {
 
 	private static IServicioEstaciones servicioEstaciones;
 	private static Repositorio<Estacion, String> repoEstaciones;
 	private static Repositorio<Bici, String> repoBicis;
 	private static Repositorio<Historico, String> repoHistoricos;
-	private FiltroBusquedaHistorico filtroHistorico 
+	private static FiltroBusquedaHistorico filtroHistorico;
 	private static Estacion estacion1;
 	private static Bici bici;
 	@BeforeAll
-	void initialize()
+	static void initialize()
 	{
 		servicioEstaciones = FactoriaServicios.getServicio(IServicioEstaciones.class);
 		repoEstaciones = FactoriaRepositorios.getRepositorio(Estacion.class);
@@ -40,12 +47,60 @@ class TestServicioEstaciones {
 		try {
 			repoEstaciones.add(estacion1);
 		} catch (RepositorioException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
 	}
 	
+	@AfterAll
+	static void end()
+	{
+		
+		try {
+			repoBicis.delete(bici);
+			repoEstaciones.delete(estacion1);
+		} catch (RepositorioException | EntidadNoEncontrada e) {
+			e.printStackTrace();
+		}
+	}
+	
+	 @Test
+	 public void pruebaServicioEstacionesDarAlta() {
+		 try
+		 {
+			String idEstacion = servicioEstaciones.altaEstacion("Estacion1", 2, 30004, 
+					new BigDecimal("47"), new BigDecimal("9"));
+			Estacion estacion = servicioEstaciones.obtenerEstacion(idEstacion);
+			assertNotEquals(null,estacion);
+			assertEquals(idEstacion, estacion.getId());
+		 }
+		 catch(Exception e)
+		 {
+			 e.printStackTrace();
+			 fail();
+		 }
+	 }
+	 
+	 @Test
+	 public void pruebaServicioEstacionesEstablecerSitiosEnEstacion() {
+		try
+		{
+			String idEstacion = servicioEstaciones.altaEstacion("Estacion1", 2, 30004, new BigDecimal("47"), new BigDecimal("9"));
+			Estacion estacion = servicioEstaciones.obtenerEstacion(idEstacion);
+			Set<SitioTuristico> set = servicioEstaciones.obtenerSitiosTuristicosProximos(idEstacion);
+			servicioEstaciones.establecerSitiosTuristicos(idEstacion, set);
+			assertNotEquals(null, estacion.getSitiosInteres());
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			fail();
+		}
+	 }
+	 
+	
 	@Test
+	@Order(1)
 	void testAltaBici() throws RepositorioException, EntidadNoEncontrada {
 		String idBici = servicioEstaciones.altaBici("BMX", estacion1.getId());
 		repoEstaciones.update(estacion1);
@@ -66,6 +121,7 @@ class TestServicioEstaciones {
 	}
 	
 	@Test
+	@Order(2)
 	void testRetirarBici() throws RepositorioException, EntidadNoEncontrada
 	{
 		servicioEstaciones.retirarBici(bici.getId());
@@ -81,6 +137,7 @@ class TestServicioEstaciones {
 	}
 	
 	@Test
+	@Order(3)
 	void testRetirarBiciYaRetirada() throws RepositorioException, EntidadNoEncontrada
 	{
 		Assertions.assertThrows(IllegalStateException.class,
@@ -88,6 +145,7 @@ class TestServicioEstaciones {
 	}
 	
 	@Test
+	@Order(4)
 	void testEstacionarBici() throws RepositorioException, EntidadNoEncontrada
 	{
 		servicioEstaciones.estacionarBici(bici.getId());
@@ -96,17 +154,25 @@ class TestServicioEstaciones {
 	}
 	
 	@Test
-	void testEstacinarBiciArgumento() throws RepositorioException, EntidadNoEncontrada
+	void testEstacionarBiciArgumento() throws RepositorioException, EntidadNoEncontrada
 	{
 		Assertions.assertThrows(IllegalArgumentException.class,
 				()->servicioEstaciones.estacionarBici(null));
 	}
 	
 	@Test
-	void testEstacinarBiciArgumento2() throws RepositorioException, EntidadNoEncontrada
+	void testEstacionarBiciArgumento2() throws RepositorioException, EntidadNoEncontrada
 	{
 		Assertions.assertThrows(IllegalArgumentException.class,
 				()->servicioEstaciones.estacionarBici(bici.getId(), null));
+	}
+	
+	@Test
+	@Order(5)
+	void testEstacionarBiciYaEstacionada() throws RepositorioException, EntidadNoEncontrada
+	{
+		Assertions.assertThrows(IllegalStateException.class,
+				()->servicioEstaciones.estacionarBici(bici.getId()));
 	}
 	
 }
