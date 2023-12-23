7595ff51857d36ef9cd5b43e66bff2b17fbc99d9
fj.perezpujalte <javiperpu@gmail.com>
2023-12-23 17:54:28 +0100

    Ficheros añadidos que faltaban

---
+++ estaciones-BarrenecheaMartinez/src/main/java/citybike/web/bici/VerIncidenciaWeb.java
@@ -0,0 +1,51 @@
+package citybike.web.bici;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import bicis.dto.IncidenciaDTO;
+import bicis.modelo.Incidencia;
+import incidencias.servicio.ServicioIncidencias;
+import servicio.FactoriaServicios;
+
+@Named 
+@ViewScoped
+public class VerIncidenciaWeb implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private ServicioIncidencias servicio;
+	private List<IncidenciaDTO> incidencias;
+	
+	@Inject
+    protected FacesContext facesContext;
+	
+	public VerIncidenciaWeb()
+	{
+		servicio = FactoriaServicios.getServicio(ServicioIncidencias.class);
+		incidencias = servicio.recuperarIncidenciasAbiertasDTO();
+	}
+	
+	public void recargarIncidencias()
+	{
+		incidencias = servicio.recuperarIncidenciasAbiertasDTO();
+	}
+
+	public List<IncidenciaDTO> getIncidencias()
+	{
+		return incidencias;
+	}
+	
+	public void setIncidencias(List<IncidenciaDTO> incidencias)
+	{
+		this.incidencias = incidencias;
+	}
+}
